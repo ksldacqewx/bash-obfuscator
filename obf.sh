9b6ae1e71209244d220c08035dfebd449cef0cdf
ksaldexwsc <62157765+ksldacqewx@users.noreply.github.com>
2020-09-19 10:08:02 +0200

    Update obf.sh

--- obf.sh
+++ obf.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
+mkdir obfuscations 2>/dev/null
 read -e -r -p "Filename: " FILE
 if ! [ -e "$FILE" ];then
 echo "File does not exist"
 exit
 fi
 echo -n "$(cat "$FILE")" | xxd -l 1000000000 -p | rev | xxd -p -r
-echo -n "$(cat "$FILE")" | xxd -l 1000000000 -p | rev | xxd -p -r >".${FILE}.OBF"
+echo -n "$(cat "$FILE")" | xxd -l 1000000000 -p | rev | xxd -p -r >"obfuscations/${FILE}.OBF"
 echo -n "#!/bin/bash
-cat \".${FILE}.OBF\" | xxd -l 10000000000000 -p | rev | xxd -p -r > .deobf.sh
+cat \"${FILE}.OBF\" | xxd -l 10000000000000 -p | rev | xxd -p -r >\".${FILE}.deobf.sh\"
 chmod +x *
 read -t 0.075
-. .deobf.sh
+. .${FILE}.deobf.sh
 read -t 0.075
-rm -rf .deobf.sh
-" >"${FILE}.Deobf.sh"
-chmod +x *
+rm -rf .${FILE}.deobf.sh
+" >"obfuscations/${FILE}.deobf.sh"
+chmod +x obfuscations/*
+echo ""
+ls obfuscations/
+echo ""
