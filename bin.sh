3c3de142a98198fd1c7877e0d9059a221674ef41
ksaldexwsc <62157765+ksldacqewx@users.noreply.github.com>
2020-10-10 09:00:19 +0200

    Add files via upload
    obfuscate binary files

---
+++ bin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir obfuscations 2>/dev/null
+read -e -r -p "Filename: " FILE
+if ! [ -e "$FILE" ];then
+echo "File does not exist"
+exit
+fi
+DATA="$(cat $FILE | base64 -w 0)"
+echo "#!/bin/bash
+echo \"$DATA\" | base64 -d > $FILE.deobf
+chmod +x $FILE.deobf
+./$FILE.deobf
+rm $FILE.deobf" > obfuscations/$FILE.sh
+chmod +x ./obfuscations/*
